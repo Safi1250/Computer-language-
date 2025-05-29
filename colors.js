4c0dddd7b46bb9a06319246b90b5f7c79ce81b98
Safi1250 <safiullahshaikh636000@gmail.com>
2025-05-29 16:03:38 +0530

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,58 @@
+export const colors = {
+  dark: {
+    background: '#1e1e1e',
+    editorBackground: '#1e1e1e',
+    sidebarBackground: '#252526',
+    statusBarBackground: '#007acc',
+    text: '#d4d4d4',
+    syntax: {
+      keyword: '#569cd6',
+      function: '#dcdcaa',
+      string: '#ce9178',
+      number: '#b5cea8',
+      comment: '#6a9955'
+    }
+  },
+  light: {
+    background: '#ffffff',
+    editorBackground: '#ffffff',
+    sidebarBackground: '#f3f3f3',
+    statusBarBackground: '#007acc',
+    text: '#333333',
+    syntax: {
+      keyword: '#0000ff',
+      function: '#795e26',
+      string: '#a31515',
+      number: '#098658',
+      comment: '#008000'
+    }
+  },
+  solarized: {
+    background: '#002b36',
+    editorBackground: '#002b36',
+    sidebarBackground: '#073642',
+    statusBarBackground: '#268bd2',
+    text: '#839496',
+    syntax: {
+      keyword: '#859900',
+      function: '#b58900',
+      string: '#2aa198',
+      number: '#d33682',
+      comment: '#586e75'
+    }
+  },
+  monokai: {
+    background: '#272822',
+    editorBackground: '#272822',
+    sidebarBackground: '#3e3d32',
+    statusBarBackground: '#75715e',
+    text: '#f8f8f2',
+    syntax: {
+      keyword: '#f92672',
+      function: '#a6e22e',
+      string: '#e6db74',
+      number: '#ae81ff',
+      comment: '#75715e'
+    }
+  }
+};
